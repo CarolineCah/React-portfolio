781c8aea92be1fc49f7dc1c97eceaf95bb3a7c26
Caroline Cah <61294176+CarolineCah@users.noreply.github.com>
2022-10-30 18:17:38 +0100

    feat: react portfolio project

---
+++ src/components/Projects.js
@@ -0,0 +1,127 @@
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import projImg1 from "../assets/img/project-img1.png";
+import projImg2 from "../assets/img/project-img2.png";
+import projImg3 from "../assets/img/project-img3.png";
+import projImg4 from "../assets/img/project-img4.png";
+import projImg5 from "../assets/img/project-img5.png";
+
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import "animate.css";
+import TrackVisibility from "react-on-screen";
+
+export const Projects = () => {
+  const webProjects = [
+    {
+      title: "Polestar.com",
+      description: "Software development",
+      imgUrl: projImg1,
+    },
+    {
+      title: "about.polestar.com",
+      description: "Software development",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Sales tools",
+      description:
+        "Software development. Web apps that displays emissions of Polestar cars compared to combustion engines",
+      imgUrl: projImg5,
+    },
+  ];
+
+  const androidProjects = [
+    {
+      title: "Android App - Västtrafik API integration",
+      description:
+        "University project. An app displaying current disturbances in the public transport in Gothenburg",
+      imgUrl: projImg4,
+    },
+  ];
+
+  const pluginProjects = [
+    {
+      title: "DatoCMS plugins",
+      description:
+        "Software Engineer. Backend in C# to filter, sort and correct incoming JSON data",
+      imgUrl: projImg3,
+    },
+  ];
+
+  return (
+    <section className="project" id="project">
+      <Container>
+        <Row>
+          <Col size={12}>
+            <TrackVisibility>
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animate__animated animate__fadeIn" : ""
+                  }
+                >
+                  <h2>Projects</h2>
+                  <a
+                    href={"https://www.polestar.com/se/polestar-2/"}
+                    style={{ color: "white" }}
+                  >
+                    Polestar.com/se/polestar-2
+                  </a>
+                  <p></p>
+                  <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                    <Nav
+                      variant="pills"
+                      className="nav-pills mb-5 justify-content-center align-items-center"
+                      id="pills-tab"
+                    >
+                      <Nav.Item>
+                        <Nav.Link eventKey="first">Web development</Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="second">
+                          Android Development
+                        </Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="third">Plugin development</Nav.Link>
+                      </Nav.Item>
+                    </Nav>
+                    <Tab.Content
+                      id="slideInUp"
+                      className={
+                        isVisible ? "animate__animated animate__slideInUp" : ""
+                      }
+                    >
+                      <Tab.Pane eventKey="first">
+                        <Row>
+                          {webProjects.map((project, index) => {
+                            return <ProjectCard key={index} {...project} />;
+                          })}
+                        </Row>
+                      </Tab.Pane>
+                      <Tab.Pane eventKey="second">
+                        <Row>
+                          {androidProjects.map((project, index) => {
+                            return <ProjectCard key={index} {...project} />;
+                          })}
+                        </Row>
+                      </Tab.Pane>
+                      <Tab.Pane eventKey="third">
+                        <Row>
+                          {pluginProjects.map((project, index) => {
+                            return <ProjectCard key={index} {...project} />;
+                          })}
+                        </Row>
+                      </Tab.Pane>
+                    </Tab.Content>
+                  </Tab.Container>
+                </div>
+              )}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-image-right" src={colorSharp2}></img>
+    </section>
+  );
+};
