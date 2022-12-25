d514a96ba83fe0b4df0bb955a3546816abf1416a
Caroline Cah <61294176+CarolineCah@users.noreply.github.com>
2022-12-25 20:57:29 +0100

    feat: change logos

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
-import logo from "../assets/img/logo4.png";
+import logo from "../assets/img/logo7.png";
 import navIcon1 from "../assets/img/nav-icon1.svg";
 import navIcon2 from "../assets/img/nav-icon2.svg";
 import navIcon3 from "../assets/img/nav-icon3.svg";
